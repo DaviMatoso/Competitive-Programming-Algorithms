6339530f302966899665d43fb9cb42bb33a7f420
samuellh12 <samuellh12cpc@gmail.com>
2026-07-26 17:44:28 -0300

    feat: add smaller fft implementation

--- Library/Math/FFT2in1.cpp
+++ Library/Math/FFT2in1.cpp
@@ -1,44 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ld double
+#define ll long long
+#define ld double //(10% slower if long double)
 typedef complex<ld> CD;
-const ld PI = acos(-1);
 
-void fft(vector<CD> &a, bool inverse=false){
+void fft(vector<CD>& a){
 	int n = a.size(), L = 31 - __builtin_clz(n);
+
+	static vector<complex<long double>> R(2, 1);
+	static vector<CD> rt(2, 1);
+	
+	for(static int k = 2; k < n; k *= 2){
+		auto x = polar(1.0L, acos(-1.0L)/k);
+		R.resize(n); rt.resize(n);
+
+		for(int i=k; i<2*k; i++) 
+			rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+	}
+
 	vector<int> rev(n);
 	for(int i=0; i<n; i++) rev[i] = (rev[i/2] | (i&1)<<L)/2;
 	for(int i=0; i<n; i++) if(i<rev[i]) swap(a[i], a[rev[i]]);
-
-	for(int k=1; k<n; k<<=1){
-		ld ang= PI / k * (inverse ? -1 : +1);
-		CD wlen(cos(ang), sin(ang));
-		for(int i=0; i<n; i+=k+k){  CD w(1);
-			for(int j=0; j<k; j++, w *= wlen){
-				CD u = a[i+j];
-				CD v = a[i+j+k] * w;
-				a[i+j] = u+v;
-				a[i+j+k] = u-v;
+	
+	for(int k=1; k<n; k*=2)
+		for(int i=0; i<n; i+=2*k)
+			for(int j=0; j<k; j++){
+				auto x=(ld*)&rt[j+k], y=(ld*)&a[i+j+k];
+				CD z (x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);
+			//  CD z = rt[j+k] * a[i+j+k]; //(~25% slower,but less code. Delete 2lines above)
+				a[i+j+k] = a[i+j] - z;
+				a[i+j] += z;
 			}
-		}
-	}
-	if(inverse) for (CD &x : a) x /= n;
 }
 
 vector<ld> conv(const vector<ld>& a, const vector<ld>& b){
-	if(a.empty() || b.empty()) return {}; //or(32-builtin_clz(m))
-	int m = a.size()+b.size()-1, n=1<<__bit_width(m-1);//ifcomp err
+	if(a.empty() || b.empty()) return {};
+	vector<ld> res(a.size() + b.size() - 1);
+	int n = 1<<(32 - __builtin_clz(res.size()));
 
-	vector<CD> fa(begin(a), end(a)); fa.resize(n);
-	vector<CD> fb(begin(b), end(b)); fb.resize(n);
+	vector<CD> in(n), out(n);
+	copy(begin(a), end(a), begin(in));
+	for(int i=0; i<b.size(); i++) in[i].imag(b[i]);
+
+	fft(in); 
+	for(auto& x : in) x *= x;
+	for(int i=0; i<n; i++) out[i] = in[-i&(n-1)] - conj(in[i]);
+	fft(out);	
 	
-	fft(fa); fft(fb);
-	for(int i=0; i<n; i++) fa[i] *= fb[i];
-	fft(fa, true);
-
-	vector<ld> ans(m);
-	for(int i=0; i<m; i++) ans[i] = fa[i].real();
-	return ans;
+	for(int i=0; i<res.size(); i++) res[i] = imag(out[i]) / (4*n);
+	return res;
 }
 
 /*LATEX_DESC_BEGIN***************************
